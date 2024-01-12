382d49206b3b43235f6049dcd49281205cbb7908
Ariel Cruz <acruzo2025@gmail.com>
2024-01-12 16:29:00 -0300

    funcion que aplica colores aleatorios

---
+++ script.js
@@ -0,0 +1,28 @@
+//Funcion para generar colores random
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+//Agrega colores random a las etiquetas con clase .col
+document.addEventListener("DOMContentLoaded", function () {
+    var colElements = document.querySelectorAll(".col");
+
+    if (colElements.length > 0) {
+        colElements.forEach(function(element) {
+            element.style.color = getRandomColor();
+        });
+    }
+});
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     var classElement = document.querySelector(".col");
+
+//     if (classElement) {
+//         classElement.style.color = getRandomColor();
+        
+//     }
+// });
